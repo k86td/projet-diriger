a0b902345fb7fcf54a3c9aa69134fec5e2338902
Tristan <tristanlepine14@gmail.com>
2022-09-21 11:39:29 -0400

    added some models

--- Infra/Models/DemandeModel.cs
+++ Infra/Models/DemandeModel.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infra.Ressources
+namespace Infra.Models
 {
 #pragma warning disable CS8618 // Un champ non-nullable doit contenir une valeur non-null lors de la fermeture du constructeur. Envisagez de déclarer le champ comme nullable.
-    public class ConversationsRessource
+    public class DemandeModel
     {
-        public int Id { get; set; }
-        public int IdAuteur { get; set; }
-        public int IdDestinataire { get; set; }
-        public string Contenu { get; set; }
+        public UsagerModel Usager { get; set; }
         public DateTime Date { get; set; }
     }
 #pragma warning restore CS8618 // Un champ non-nullable doit contenir une valeur non-null lors de la fermeture du constructeur. Envisagez de déclarer le champ comme nullable.
