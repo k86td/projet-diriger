735b15c54242e2ffe29ca0878caa38e121584734
Tristan <40375233+k86td@users.noreply.github.com>
2022-09-20 23:28:25 -0400

    started creating models and ressources

---
+++ Infra/Models/ConversationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Models
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public class ConversationModel
+    {
+        public int Id { get; set; }
+        public UsagerModel Auteur { get; set; }
+        public UsagerModel Destinataire { get; set; }
+        public string Contenu { get; set; }
+        public DateTime Date { get; set; }
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+}
