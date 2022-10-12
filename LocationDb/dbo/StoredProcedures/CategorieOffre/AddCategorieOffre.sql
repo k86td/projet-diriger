f7e475608b29fc214bc380e7683f8f75aca64b00
Tristan <tristanlepine14@gmail.com>
2022-10-12 16:50:16 -0400

    Commit changes

---
+++ LocationDb/dbo/StoredProcedures/CategorieOffre/AddCategorieOffre.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[AddCategorieOffre]
+	@IdType INT,
+	@Nom VARCHAR(30),
+	@IdCategorieOffre INT OUT
+AS
+BEGIN
+	INSERT INTO dbo.CategoriesOffres (IdType, Nom)
+	VALUES (@IdType, @Nom);
+
+	SET @IdCategorieOffre = SCOPE_IDENTITY();
+END;
