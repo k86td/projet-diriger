13f5d0bd6e409aa005a99bc02e59abee27754a85
Guillaume <guillaume.richer@bell.net>
2022-11-19 14:46:45 -0500

    post endpoint add conversation

--- Api/Controllers/ConversationsController.cs
+++ Api/Controllers/ConversationsController.cs
@@ -2,6 +2,7 @@
 using Infra.Dal.Interfaces;
 using Infra.Ressources;
 using Infra.Dal.Implementations;
+using Microsoft.Extensions.Logging;
 
 namespace Api.Controllers
 {
@@ -13,12 +14,26 @@ namespace Api.Controllers
         public ConversationsController(IConversationsData ConversationsData)
         {
             _ConversationsData = ConversationsData;
+           
         }
         [HttpGet]
         public async Task<IEnumerable<ConversationsRessource>> Get()
         {
             return await _ConversationsData.Get();
         }
-      
+        [HttpPost]
+        public async Task<IActionResult> Post(ConversationsRessource conversation)
+        {
+            try
+            {
+                await _ConversationsData.Create(conversation);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
     }
 }
